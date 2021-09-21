f9a6f57ecb15aac0b06a61fde67deef42c51f4e3
tutty.ben <tutty.ben@awatapu.school.nz>
2021-09-21 13:02:32 +1200

    num check

--- venv/Number checker.py
+++ venv/Number checker.py
@@ -1,16 +1,15 @@
 #
+number_check(question,low,high):
+    error = "Please enter a whole number between 1 and 10\n"
 
-error = "Please enter a whole number between 1 and 10\n"
+    valid = False
+    while not valid:
+        try:
+            response = int(input("How much would you like to play with? "))
+            if 0 < response <= 10:
+                print("You have asked to play with ${}".format(response))
+            else:
+                print(error)
 
-valid = False
-while not valid:
-    try:
-        response = int(input("How much would you like to play with? "))
-        if 0 < response <= 10:
-            print("You have asked to play with ${}".format(response))
-
-        else:
-            print(error)
-
-    except ValueError:
-            print(error)
+        except ValueError:
+                print(error)
